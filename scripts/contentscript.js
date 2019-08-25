6810afe7808a7522cae9ca0aaca970101d4a7b14
Fabian <fabian@familie-palitza.de>
2019-08-25 03:40:17 +0200

    hotfix sorting and filtering
    fix problems in contentscript.js:
        sorting by champion, region or author and filtering by author

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -84,7 +84,7 @@ window.addEventListener("load", function() {
                     break;
                 
                 case "authors":
-                    var maxIndex = Math.min(a.authors.length, b.authors.length);
+                    var maxIndex = Math.min(a.tags.authors.length, b.tags.authors.length);
                     for (i = 0; i < maxIndex; i++) {
                         if (a.authors[i].localeCompare(b.authors[i]) != 0) {
                             return a.authors[i].localeCompare(b.authors[i]);
@@ -95,25 +95,25 @@ window.addEventListener("load", function() {
                     break;
                 
                 case "champions":
-                    var maxIndex = Math.min(a.champions.length, b.champions.length);
+                    var maxIndex = Math.min(a.tags.champions.length, b.tags.champions.length);
                     for (i = 0; i < maxIndex; i++) {
-                        if (a.champions[i].localeCompare(b.champions[i]) != 0) {
-                            return a.champions[i].localeCompare(b.champions[i]);
+                        if (a.tags.champions[i].localeCompare(b.tags.champions[i]) != 0) {
+                            return a.tags.champions[i].localeCompare(b.tags.champions[i]);
                             break;
                         }
                     }
-                    return a.champions.length - b.champions.length;
+                    return a.tags.champions.length - b.tags.champions.length;
                     break;
                 
                 case "regions":
-                    var maxIndex = Math.min(a.regions.length, b.regions.length);
+                    var maxIndex = Math.min(a.tags.regions.length, b.tags.regions.length);
                     for (i = 0; i < maxIndex; i++) {
-                        if (a.regions[i].localeCompare(b.regions[i]) != 0) {
-                            return a.regions[i].localeCompare(b.regions[i]);
+                        if (a.tags.regions[i].localeCompare(b.tags.regions[i]) != 0) {
+                            return a.regions[i].localeCompare(b.tags.regions[i]);
                             break;
                         }
                     }
-                    return a.regions.length - b.regions.length;
+                    return a.tags.regions.length - b.tags.regions.length;
                     break;
                     
                 default: 
@@ -180,7 +180,7 @@ window.addEventListener("load", function() {
                 }
             }
             if (document.getElementById("uek-filter-authors").checked) {
-                const author = document.getElementById("uek-filter-regions-dropdown").value;
+                const author = document.getElementById("uek-filter-authors-dropdown").value;
                 if (author != "") {
                     currentSelection = currentSelection.filter(a => a.tags.authors.includes(author));
                 }
